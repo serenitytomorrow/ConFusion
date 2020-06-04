f8b0619547e73862295d88b637332f169115d7a5
DP <websiteservices@protonmail.com>
2020-06-04 16:18:57 -0400

    configuring react

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Navbar, NavbarBrand } from 'reactstrap';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+      </div>
+    );
+  }
+}
