797d58d4726399dd3785f73554c4e92ba80f5f08
DP <websiteservices@protonmail.com>
2020-06-05 09:32:40 -0400

    Components Part 2

--- src/App.js
+++ src/App.js
@@ -3,8 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent';
+import { DISHES } from './shared/dishes';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
   render() {
     return (
       <div className="App">
@@ -13,7 +20,7 @@ export default class App extends React.Component {
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
-         <Menu />
+        <Menu dishes={this.state.dishes} />
       </div>
     );
   }
