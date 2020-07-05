5844cf485cb6a53614e0d35f3493058dc8481bb8
DP <websiteservices@protonmail.com>
2020-07-05 10:35:27 -0400

    React Animation Components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -6,6 +6,7 @@ import { Card, CardImg, CardText, CardBody,
 import { Link } from 'react-router-dom';
 import CommentForm from './CommentForm';
 import { baseUrl } from '../shared/baseUrl';
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
 
 
 
@@ -15,10 +16,18 @@ function RenderDish({dish}) {
 function RenderComments({comments, postComment, dishId}) {
     return comments.map((comment) => {
         return (
-            <li key={'dishdetail-'+ comment.id}>
-                <i>{comment.comment}</i>
-                <p>--{comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
-            </li>
+            <Stagger in>
+                {comments.map((comment) => {
+                    return (
+                        <Fade in>
+                        <li key={comment.id}>
+                        <p>{comment.comment}</p>
+                        <p>-- {comment.author} , {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
+                        </li>
+                        </Fade>
+                    );
+                })}
+                </Stagger>
         )});
 }
  const  DishDetail = (props) => {
@@ -60,7 +69,19 @@ function RenderComments({comments, postComment, dishId}) {
             <div className="row">
                 <div className="col-12 col-md-5 m-1">
                     <RenderDish dish={props.dish} />
-                    <CardImg top src={baseUrl + props.dish.image} alt={props.dish.name} />
+                    <FadeTransform
+                        in
+                        transformProps={{
+                            exitTransform: 'scale(0.5) translateY(-50%)'
+                        }}>
+                    <Card>
+                        <CardImg top src={baseUrl + props.dish.image} alt={props.dish.name} />
+                        <CardBody>
+                            <CardTitle>{props.dish.name}</CardTitle>
+                            <CardText>{props.dish.description}</CardText>
+                        </CardBody>
+                    </Card>
+                    </FadeTransform>
                 </div>
                 <div className="col-12 col-md-5 m-1">
                 <RenderComments comments={props.comments} postComment={props.postComment} dishId={props.dish.id} />
